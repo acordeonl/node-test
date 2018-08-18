cf65fd3c6e1d67a4e03255abba710ea13ce32ba2
acordeonl <acordeonlwebmaster@gmail.com>
2018-08-18 14:17:46 -0500

    add login logout logic

--- routes/main.js
+++ routes/main.js
@@ -4,10 +4,14 @@ var router = express.Router();
 var userAuth = require('../route-helpers/auth');
 var verifyUser =  userAuth.verifyUser ;
 
-router.post('/auth', async function (req, res, next) {
-    console.log(req.body.user);
-    console.log(req.body.pass);
-    req.redirect('/dashboard') ; 
+router.post('/login', async function (req, res, next) {
+    console.log('in login');
+    if(req.body.user === 'user' && req.body.pass === 'pass')
+        res.redirect('/dashboard') ; 
+    else if(req.body.user !== undefined)
+        res.render('login',{response:'Unauthorized'}) ; 
+    else 
+        res.render('login',{response:''}) ; 
     // try {
     //     let response = (await req.db.collection('Users').find({
     //         givenName: req.body.firstname
@@ -21,14 +25,19 @@ router.post('/auth', async function (req, res, next) {
     // }
 });
 
+router.get('/login', function (req, res, next) {
+    res.render('login' , {response:''}) ; 
+});
+
 
 router.get('/', function (req, res, next) {
     res.redirect('/login');
-})
+}) ; 
 
-router.get('/login', function (req, res, next) {
-    res.render('login',{response:''});
-});
+router.get('/logout', function (req, res, next) {
+    console.log('in log out');
+    res.redirect('/login');
+}) ; 
 
 router.get('/dashboard', function (req, res, next) {
     res.render('dashboard') ; 
