34510f84599e648486cb0523b4129fd45d6c4b1f
acordeonl <acordeonlwebmaster@gmail.com>
2018-08-18 12:51:29 -0500

    add user login post test

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,24 @@ var router = express.Router();
 var userAuth = require('../route-helpers/auth');
 var verifyUser =  userAuth.verifyUser ;
 
+router.post('/auth', async function (req, res, next) {
+    console.log(req.body.user);
+    console.log(req.body.pass);
+    req.redirect('/dashboard') ; 
+    // try {
+    //     let response = (await req.db.collection('Users').find({
+    //         givenName: req.body.firstname
+    //     }).toArray())[0];
+    //     let getResponse = await fetch('http://localhost:3000/v1/getTest');
+    //     console.log(getResponse, response);
+    //     res.send(200);
+    //     // next() ;
+    // } catch (err) {
+    //     return next(err);
+    // }
+});
+
+
 router.get('/', function (req, res, next) {
     res.redirect('/login');
 })
@@ -15,5 +33,7 @@ router.get('/login', function (req, res, next) {
 router.get('/dashboard', function (req, res, next) {
     res.render('dashboard') ; 
 });
+
+
 
 module.exports = router;
