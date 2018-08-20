aa5fba2a0613c6d6938ceeadd8367b6724060751
acordeonl <acordeonlwebmaster@gmail.com>
2018-08-19 23:05:10 -0500

    add swagger CRUD documentation for api

--- route-helpers/auth.js
+++ route-helpers/auth.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var verifyUser = async function (req, res, next) {
-    // if(!req.user){
-    //     res.redirect('/login') ; 
-    // }
-    // else{
+    if(!req.user){
+        res.redirect('/login') ; 
+    }
+    else{
         next() ; 
-    // }
+    }
 }
 
 module.exports.verifyUser = verifyUser;
